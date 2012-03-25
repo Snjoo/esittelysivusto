8611f0e83958290b451ce9b2283d3a7aa0f39ee1
Snjoo <snjoo@snjoo-pc.(none)>
2012-03-25 23:26:52 +0300

    eka commit

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', function(req, res) {
+    res.render('index', {
+	title: 'Pääsivu'
+    });
+});
+
+app.get('/codes', function(req, res) {
+    res.render('codes', {
+	title: 'Lähdekoodeja'
+    });
+});
+
+app.get('/me', function(req, res) {
+    res.render('me', {
+	title: 'Tietoja minusta'
+    });
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact', {
+	title: 'Yhteystiedot'
+    });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
